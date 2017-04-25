914047c33ba1658c6563f11bcb801e5a18b10af5
wrgl0 <marcus.wargloo@gmail.com>
2017-04-25 12:01:07 +0200

    indentation fix (testing)

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -13,16 +13,16 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-    	ObjectNode result = Json.newObject();
-    	result.put("content", "Test Test Test");
-        return ok(result);
-    }
+	/**
+	 * An action that renders an HTML page with a welcome message.
+	 * The configuration in the <code>routes</code> file means that
+	 * this method will be called when the application receives a
+	 * <code>GET</code> request with a path of <code>/</code>.
+	 */
+	public Result index() {
+		ObjectNode result = Json.newObject();
+		result.put("content", "Test Test Test");
+		return ok(result);
+	}
 
 }
--- build.sbt
+++ build.sbt
@@ -29,3 +29,5 @@ initialize := {
   if (sys.props("java.specification.version") != "1.8")
     sys.error("Java 8 is required for this project.")
 }
+
+fork in run := true
