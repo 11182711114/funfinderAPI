629935d6469516da7a39307a8972c199f560bca6
Fredrik <Fredrik@DESKTOP-QPEG16D>
2017-04-27 17:30:50 +0200

    Add column names to ORM

--- app/models/User.java
+++ app/models/User.java
@@ -6,20 +6,31 @@ import com.avaje.ebean.Model;
 import play.data.validation.*;
 
 @Entity
+@Table(name = "User")
 public class User extends Model{
-
+	
 	@Id
+	@Column(name = "idUser")
 	public Long id;
 	
 	@Constraints.Required
+	@Column(name = "firstName")
 	public String firstName;
 
 	@Constraints.Required
+	@Column(name = "lastName")
 	public String lastName;
-	
+
+	@Column(name = "age")
 	public int age;
-	
+
+	@Column(name = "email")
 	public String email;
+	
+	public User(String fName, String lName) {
+		this.firstName = fName;
+		this.lastName = lName;
+	}
 	
 	public static Finder<Long, User> find = new Finder<Long,User>(User.class);
 }
