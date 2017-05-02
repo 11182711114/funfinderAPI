a95e97a219b5d08e840a5953ca55f512f8201a61
fredrik <fredrik@DESKTOP-FLEU76S>
2017-05-02 11:42:09 +0200

    Fix db connection, use evolutions

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,19 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table user (
+  iduser                        integer auto_increment not null,
+  firstname                     varchar(255),
+  lastname                      varchar(255),
+  age                           integer,
+  email                         varchar(255),
+  constraint pk_user primary key (iduser)
+);
+
+
+# --- !Downs
+
+drop table if exists user;
+
