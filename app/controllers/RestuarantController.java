1c7ad4250763aa8fb2a4403d4a8a440fefc2d8b6
wrgl0 <marcus.wargloo@gmail.com>
2017-05-17 00:12:18 +0200

    fixes

--- app/controllers/RestuarantController.java
+++ app/controllers/RestuarantController.java
@@ -17,7 +17,7 @@ public class RestuarantController extends Controller {
 
 
 	public Result getRestaurantById(String id) {
-		Restaurant rest = Restaurant.find.byId(Long.parseLong(id));
+		Restaurant rest = Restaurant.find.byId(id);
 
 		return rest != null ? ok(Json.toJson(rest)) : notFound();
 	}
@@ -30,12 +30,12 @@ public class RestuarantController extends Controller {
 	public void getRestaurantsNearby(double lat, double lng, int rad){
 		ResultParser respesp = new ResultParser();
 		ArrayList<parser.Restaurant> results = respesp.searchNearby(lat, lng, rad);
-		
+
 		for(parser.Restaurant rest : results){
 			String name = rest.getName();
 			double rating = rest.getRating();
 			String id = rest.getId();
-			
+
 			Restaurant newRestaurant = new Restaurant(id, name, rating);
 			try {
 				newRestaurant.save();
@@ -43,37 +43,25 @@ public class RestuarantController extends Controller {
 				pe.printStackTrace();
 			}
 		}
-//		PrettyPrinted Json (use this while testing)
-//		JsonNode node = Json.toJson(results);
-//		ObjectMapper mapper = new ObjectMapper();
-//		try {
-//			String pretty = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(node);
-//			return ok(pretty);
-//		} catch (JsonProcessingException e) {
-//			e.printStackTrace();
-//		}
-//		return null;
-		//		Clean JSON response
-		//		return results != null ? ok(Json.toJson(results)) : notFound();
 	}
 
 	public void getRestaurantsByLocation(String location){
 		ResultParser respesp = new ResultParser();
 		ArrayList<parser.Restaurant> results = respesp.searchText(location);
 
-		
+
 		//		Clean JSON response
 		//		return results != null ? ok(Json.toJson(results)) : notFound();
 	}
 
-	public Result getRestaurant(String id){
-		Restaurant rest = Restaurant.find.byId(id);
-		if(rest == null)
-			return notFound("Restaurant doesn't exist");
-		String json = "{\"uid\":" + rest.getId() + ",\"name\":\"" + rest.getName() + "\",\"rating\":\"" + rest.getRating() + "\"}";
-		
-		JsonNode jn = Json.parse(json);
-		return ok(jn);
-	}
-		
+	//	public Result getRestaurant(String id){
+	//		Restaurant rest = Restaurant.find.byId(id);
+	//		if(rest == null)
+	//			return notFound("Restaurant doesn't exist");
+	//		String json = "{\"uid\":" + rest.getId() + ",\"name\":\"" + rest.getName() + "\",\"rating\":\"" + rest.getRating() + "\"}";
+	//		
+	//		JsonNode jn = Json.parse(json);
+	//		return ok(jn);
+	//	}
+
 }
