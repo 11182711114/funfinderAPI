1348dbea2ffe634ed8886fea1c1ff7f8b11c9c36
wrgl0 <marcus.wargloo@gmail.com>
2017-05-19 10:27:29 +0200

    better GET-method names, added documentation to methods

--- app/controllers/RestuarantController.java
+++ app/controllers/RestuarantController.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.*;
 
 import models.Location;
 import models.Restaurant;
+import models.User;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -20,37 +21,30 @@ import parser.*;
 public class RestuarantController extends Controller {
 
 
-	public Result getRestaurantById(String id) {
-		Restaurant rest = Restaurant.find.byId(id);
 
-		return rest != null ? ok(Json.toJson(rest)) : notFound();
-	}
-
-	/*
-	 * Results requested by client are not to be saved into the database but only parsed and 
-	 *  forwarded to the client.
-	 */
-
+/**
+ * Gets restaurant by location, saves in db
+ * @param userLat - user lattitude
+ * @param userLng - user longitude
+ * @param searchRadie - radie from the users location to search
+ * 
+ * @return successmessage if successfully loaded to db or failmessage
+ */
 	public Result getRestaurantsNearby(double userLat, double userLng, int searchRadie){
 		ResultParser respesp = new ResultParser();
 		ArrayList<parser.ParsedRestaurant> results = respesp.searchNearby(userLat, userLng, searchRadie);
 
 		for(ParsedRestaurant pl : results){
-
-//			System.out.println("ADDING: "+pl.toString()+" "+pl.getLocation().getAddress());//TODO remove testline
-			
 			String adress = pl.getLocation().getAddress();
 			double lat = pl.getLocation().getLattitude();
 			double lng = pl.getLocation().getLongitude();
 			Location locat = new Location(adress, lat, lng);
 			Ebean.save(locat);
-		
+
 			String name = pl.getName();
 			double rating = pl.getRating();
 			String id = pl.getId();
 			String locid = ""+locat.getId();
-//			System.out.println(">>>LOC ID : "+locid); //TODO remove testline
-			
 			Restaurant newRestaurant = new Restaurant(id, name, rating, locid);
 			try {
 				newRestaurant.save();
@@ -62,26 +56,30 @@ public class RestuarantController extends Controller {
 		return ok("SUCESSFULLY EXECUTED COMMAND");
 	}
 
-	public Result getRestaurantsByLocation(String location){
+	/**
+	 * Gets restaurants nearby som text search,saves in db
+	 * 
+	 * @param textSearch - the text input from user
+	 * 			this can also be used to search on restaurants name and it will return
+	 * 			 all restaurants by that name in sthlm
+	 * 
+	 * @return successmessage if successfully loaded to db or failmessage
+	 */
+	public Result getRestaurantsByText(String textSearch){
 		ResultParser respesp = new ResultParser();
-		ArrayList<parser.ParsedRestaurant> results = respesp.searchNearby(userLat, userLng, searchRadie);
+		ArrayList<parser.ParsedRestaurant> results = respesp.searchText(textSearch);
 
 		for(ParsedRestaurant pl : results){
-
-//			System.out.println("ADDING: "+pl.toString()+" "+pl.getLocation().getAddress());//TODO remove testline
-			
 			String adress = pl.getLocation().getAddress();
 			double lat = pl.getLocation().getLattitude();
 			double lng = pl.getLocation().getLongitude();
 			Location locat = new Location(adress, lat, lng);
 			Ebean.save(locat);
-		
+
 			String name = pl.getName();
 			double rating = pl.getRating();
 			String id = pl.getId();
 			String locid = ""+locat.getId();
-//			System.out.println(">>>LOC ID : "+locid); //TODO remove testline
-			
 			Restaurant newRestaurant = new Restaurant(id, name, rating, locid);
 			try {
 				newRestaurant.save();
@@ -94,15 +92,34 @@ public class RestuarantController extends Controller {
 	}
 
 
-	
-	//	public Result getRestaurant(String id){
-	//		Restaurant rest = Restaurant.find.byId(id);
-	//		if(rest == null)
-	//			return notFound("Restaurant doesn't exist");
-	//		String json = "{\"uid\":" + rest.getId() + ",\"name\":\"" + rest.getName() + "\",\"rating\":\"" + rest.getRating() + "\"}";
-	//		
-	//		JsonNode jn = Json.parse(json);
-	//		return ok(jn);
-	//	}
+
+	/**
+	 * finds restaurant by located id
+	 * @param id 
+	 * @return restaurant as json, or not found message
+	 */
+	public Result getRestaurantById(String id){
+		Restaurant rest = Restaurant.find.byId(id);
+		if(rest == null)
+			return notFound("Restaurant doesn't exist");
+		String json = "{\"restid\":" + rest.getId() + ",\"name\":\"" + rest.getName() + "\",\"rating\":\"" + rest.getRating() + "\"}";
+
+		JsonNode jn = Json.parse(json);
+		return ok(jn);
+	}
+
+	/**
+	 * finds restaurant by name of the restaurant
+	 * @param name - of the restaurant
+	 * @return restaurant result as json, or not found message
+	 */
+	public Result getRestaurantByName(String name) {
+		Restaurant rest = Ebean.find(Restaurant.class).where().eq("name", name).findUnique();
+
+		if (rest == null)
+			return notFound("restaurant not found");
+		JsonNode result = Json.toJson(rest);
+		return ok(result);
+	}
 
 }
